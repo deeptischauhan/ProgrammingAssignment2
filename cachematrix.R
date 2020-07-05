54fbc2e51b35afa1367f524ac165bb9c495312f5
Deepti Chauhan <deepti.chauhan.dce@gmail.com>
2020-07-05 19:51:54 +0530

    Get in the inverse of a matrix from cache. If not available then calculate it and return.

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,9 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function makeCacheMatrix creates a list containing a function to 
+##1. set the value of the matrix 2. get the value of the matrix
+##3. set the inverse of the matrix 4. get the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
@@ -19,7 +21,8 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated, then the cachesolve retrieves the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
